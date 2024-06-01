4786944f4091a5e280fd9668e4340a07f6459887
Igga96 <igorjbk@mail.ru>
2024-06-01 05:26:18 +0300

    fix vue-config for deploy on gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/rick-and-morty/' : '/',
+  outputDir: 'dist',
+  filenameHashing: false,
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'rick-and-morty';
+        return args;
+      });
+  }
+}
